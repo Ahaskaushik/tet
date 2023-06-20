72cddfda1f8d02fe198d5a4b1b2ba9c7bdb76072
Ahaskaushik <129049250+Ahaskaushik@users.noreply.github.com>
2023-06-20 19:54:19 +0530

    Update tet.java

--- tet.java
+++ tet.java
@@ -2,6 +2,7 @@ public class tet{
 	public static void mian (String [] args){
 		System.out.println("Hello world");
 		System.out.println("change 2");
+		System.out.println("changes done in git hub");
 	}
 }
 
